645e98405cf1085be9c0884ce32ce308f4157f43
Raphael Rapozo Rebello de Rezende <123587313+raphaelrrrezende@users.noreply.github.com>
2023-06-07 10:04:34 -0300

    Initial commit

---
+++ app/validacao.js
@@ -0,0 +1,71 @@
+function verificarValor(chute) {
+
+    const numero = +chute;
+
+    if(chuteInvalido(numero)) {
+
+        if(chute.toUpperCase() === 'GAME OVER') {
+
+            document.body.innerHTML = `
+                <h2>Game Over</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar-game-over">Jogar novamente</button>
+            `;
+
+            document.body.style.backgroundColor = 'black';
+            document.body.style.color = 'white';
+
+        } else {
+            elementoChute.innerHTML += '<div>Valor inválido</div>';
+        }
+        
+        return;
+
+    }
+
+    if(numeroNaoPermitido(numero)) {
+        
+        elementoChute.innerHTML += `
+            <div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>
+        `;
+        return;
+
+    }
+
+    if(numero === numeroSecreto) {
+
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `;
+
+    } else if(numero > numeroSecreto) {
+
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>
+        `;
+        
+    } else {
+
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>
+        `;
+
+    }
+
+};
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero);
+};
+
+function numeroNaoPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+};
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+});
